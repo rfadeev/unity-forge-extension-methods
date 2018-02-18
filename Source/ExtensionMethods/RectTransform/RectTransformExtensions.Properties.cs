c83a2e9fb95dba91f8ff2b329865c516d62309d0
Roman Fadeev <roma.fadeev@gmail.com>
2018-02-18 21:00:39 +0800

    Extract Vector2 components management to utils class

--- Source/ExtensionMethods/RectTransform/RectTransformExtensions.Properties.cs
+++ Source/ExtensionMethods/RectTransform/RectTransformExtensions.Properties.cs
@@ -6,82 +6,72 @@ namespace UnityForge
     {
         public static void SetAnchoredPositionX(this RectTransform rectTransform, float value)
         {
-            rectTransform.anchoredPosition = SetVectorX(rectTransform.anchoredPosition, value);
+            rectTransform.anchoredPosition = Vector2Utils.SetVectorX(rectTransform.anchoredPosition, value);
         }
 
         public static void SetAnchoredPositionY(this RectTransform rectTransform, float value)
         {
-            rectTransform.anchoredPosition = SetVectorY(rectTransform.anchoredPosition, value);
+            rectTransform.anchoredPosition = Vector2Utils.SetVectorY(rectTransform.anchoredPosition, value);
         }
 
         public static void SetAnchorMaxX(this RectTransform rectTransform, float value)
         {
-            rectTransform.anchorMax = SetVectorX(rectTransform.anchorMax, value);
+            rectTransform.anchorMax = Vector2Utils.SetVectorX(rectTransform.anchorMax, value);
         }
 
         public static void SetAnchorMaxY(this RectTransform rectTransform, float value)
         {
-            rectTransform.anchorMax = SetVectorY(rectTransform.anchorMax, value);
+            rectTransform.anchorMax = Vector2Utils.SetVectorY(rectTransform.anchorMax, value);
         }
 
         public static void SetAnchorMinX(this RectTransform rectTransform, float value)
         {
-            rectTransform.anchorMin = SetVectorX(rectTransform.anchorMin, value);
+            rectTransform.anchorMin = Vector2Utils.SetVectorX(rectTransform.anchorMin, value);
         }
 
         public static void SetAnchorMinY(this RectTransform rectTransform, float value)
         {
-            rectTransform.anchorMin = SetVectorY(rectTransform.anchorMin, value);
+            rectTransform.anchorMin = Vector2Utils.SetVectorY(rectTransform.anchorMin, value);
         }
 
         public static void SetOffsetMaxX(this RectTransform rectTransform, float value)
         {
-            rectTransform.offsetMax = SetVectorX(rectTransform.offsetMax, value);
+            rectTransform.offsetMax = Vector2Utils.SetVectorX(rectTransform.offsetMax, value);
         }
 
         public static void SetOffsetMaxY(this RectTransform rectTransform, float value)
         {
-            rectTransform.offsetMax = SetVectorY(rectTransform.offsetMax, value);
+            rectTransform.offsetMax = Vector2Utils.SetVectorY(rectTransform.offsetMax, value);
         }
 
         public static void SetOffsetMinX(this RectTransform rectTransform, float value)
         {
-            rectTransform.offsetMin = SetVectorX(rectTransform.offsetMin, value);
+            rectTransform.offsetMin = Vector2Utils.SetVectorX(rectTransform.offsetMin, value);
         }
 
         public static void SetOffsetMinY(this RectTransform rectTransform, float value)
         {
-            rectTransform.offsetMin = SetVectorY(rectTransform.offsetMin, value);
+            rectTransform.offsetMin = Vector2Utils.SetVectorY(rectTransform.offsetMin, value);
         }
 
         public static void SetPivotX(this RectTransform rectTransform, float value)
         {
-            rectTransform.pivot = SetVectorX(rectTransform.pivot, value);
+            rectTransform.pivot = Vector2Utils.SetVectorX(rectTransform.pivot, value);
         }
 
         public static void SetPivotY(this RectTransform rectTransform, float value)
         {
-            rectTransform.pivot = SetVectorY(rectTransform.pivot, value);
+            rectTransform.pivot = Vector2Utils.SetVectorY(rectTransform.pivot, value);
         }
 
         public static void SetSizeDeltaX(this RectTransform rectTransform, float value)
         {
-            rectTransform.sizeDelta = SetVectorX(rectTransform.sizeDelta, value);
+            rectTransform.sizeDelta = Vector2Utils.SetVectorX(rectTransform.sizeDelta, value);
         }
 
         public static void SetSizeDeltaY(this RectTransform rectTransform, float value)
         {
-            rectTransform.sizeDelta = SetVectorY(rectTransform.sizeDelta, value);
-        }
-
-        private static Vector2 SetVectorX(Vector2 vector, float value)
-        {
-            return new Vector2(value, vector.y);
-        }
-
-        private static Vector2 SetVectorY(Vector2 vector, float value)
-        {
-            return new Vector2(vector.x, value);
+            rectTransform.sizeDelta = Vector2Utils.SetVectorY(rectTransform.sizeDelta, value);
         }
     }
 }
---
+++ Source/Utils/Vector2Utils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UnityForge
+{
+    public static class Vector2Utils
+    {
+        public static Vector2 SetVectorX(Vector2 vector, float value)
+        {
+            return new Vector2(value, vector.y);
+        }
+
+        public static Vector2 SetVectorY(Vector2 vector, float value)
+        {
+            return new Vector2(vector.x, value);
+        }
+    }
+}
