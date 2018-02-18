aae4bed7803d87e036f84d173ee6dccb2acc4e72
Roman Fadeev <roma.fadeev@gmail.com>
2018-02-18 21:11:34 +0800

    Add Transform position extension methods
    Add Vector3 utils. Implement extensions for setting world and
    local position components.

--- Source/ExtensionMethods/Transform/TransformExtentions.cs
+++ Source/ExtensionMethods/Transform/TransformExtentions.cs
@@ -18,18 +18,18 @@ namespace UnityForge
         /// </summary>
         /// <param name="target">Object to point away from.</param>
         /// <param name="worldUp">Vector specifying the upward direction.</param>
-        public static void LookFrom(this Transform trans, Transform target, Vector3 worldUp)
+        public static void LookFrom(this Transform transform, Transform target, Vector3 worldUp)
         {
-            trans.LookFrom(target.position, worldUp);
+            transform.LookFrom(target.position, worldUp);
         }
 
         /// <summary>
         /// Rotates the transform so the forward vector points from worldPosition.
         /// </summary>
         /// <param name="worldPosition">Point to look at.</param>
-        public static void LookFrom(this Transform trans, Vector3 worldPosition)
+        public static void LookFrom(this Transform transform, Vector3 worldPosition)
         {
-            trans.rotation = Quaternion.LookRotation(trans.position - worldPosition);
+            transform.rotation = Quaternion.LookRotation(transform.position - worldPosition);
         }
 
         /// <summary>
@@ -37,9 +37,63 @@ namespace UnityForge
         /// </summary>
         /// <param name="worldPosition">Point to look at.</param>
         /// <param name="worldUp">Vector specifying the upward direction.</param>
-        public static void LookFrom(this Transform trans, Vector3 worldPosition, Vector3 worldUp)
+        public static void LookFrom(this Transform transform, Vector3 worldPosition, Vector3 worldUp)
         {
-            trans.rotation = Quaternion.LookRotation(trans.position - worldPosition, worldUp);
+            transform.rotation = Quaternion.LookRotation(transform.position - worldPosition, worldUp);
+        }
+
+        /// <summary>
+        /// Sets the transform world space position x component.
+        /// </summary>
+        /// <param name="value">Value of x component.</param>
+        public static void SetPositionX(this Transform transform, float value)
+        {
+            transform.position = Vector3Utils.SetVectorX(transform.position, value);
+        }
+
+        /// <summary>
+        /// Sets the transform world space position y component.
+        /// </summary>
+        /// <param name="value">Value of y component.</param>
+        public static void SetPositionY(this Transform transform, float value)
+        {
+            transform.position = Vector3Utils.SetVectorY(transform.position, value);
+        }
+
+        /// <summary>
+        /// Sets the transform world space position z component.
+        /// </summary>
+        /// <param name="value">Value of z component.</param>
+        public static void SetPositionZ(this Transform transform, float value)
+        {
+            transform.position = Vector3Utils.SetVectorZ(transform.position, value);
+        }
+
+        /// <summary>
+        /// Sets the transform local space position x component.
+        /// </summary>
+        /// <param name="value">Value of x component.</param>
+        public static void SetLocalPositionX(this Transform transform, float value)
+        {
+            transform.localPosition = Vector3Utils.SetVectorX(transform.localPosition, value);
+        }
+
+        /// <summary>
+        /// Sets the transform local space position y component.
+        /// </summary>
+        /// <param name="value">Value of y component.</param>
+        public static void SetLocalPositionY(this Transform transform, float value)
+        {
+            transform.localPosition = Vector3Utils.SetVectorY(transform.localPosition, value);
+        }
+
+        /// <summary>
+        /// Sets the transform world space position z component.
+        /// </summary>
+        /// <param name="value">Value of z component.</param>
+        public static void SetLocalPositionZ(this Transform transform, float value)
+        {
+            transform.localPosition = Vector3Utils.SetVectorZ(transform.localPosition, value);
         }
     }
 }
---
+++ Source/Utils/Vector3Utils.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UnityForge
+{
+    public static class Vector3Utils
+    {
+        public static Vector3 SetVectorX(Vector3 vector, float value)
+        {
+            return new Vector3(value, vector.y, vector.z);
+        }
+
+        public static Vector3 SetVectorY(Vector3 vector, float value)
+        {
+            return new Vector3(vector.x, value, vector.z);
+        }
+
+        public static Vector3 SetVectorZ(Vector3 vector, float value)
+        {
+            return new Vector3(vector.x, vector.y, value);
+        }
+    }
+}
